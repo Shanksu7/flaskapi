051c9d9e665b901098aeedbc79b5b5dd7f605fe2
Juan Pablo Cortés Mora <juanp_cm97@hotmail.com>
2025-01-21 12:21:44 -0500

    oki

---
+++ first_time_setup.sh
@@ -0,0 +1,14 @@
+python3 -m venv /home/venv
+source "/home/venv/bin/activate"
+echo -e "deb http://archive.debian.org/debian stretch main contrib non-free\ndeb http://archive.debian.org/debian-security stretch/updates main contrib non-free" | tee /etc/apt/sources.list > /dev/null
+apt-get update
+apt-get install -y git
+git clone https://github.com/Shanksu7/flaskapi app
+cd app
+git pull
+cp -rf /home/site/wwwroot/app/* /home/site/wwwroot/
+cd /home/site/wwwroot/
+rm -r app
+chmod -x startup.sh
+pip install -r requirements.txt
+deactivate
